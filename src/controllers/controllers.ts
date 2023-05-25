150be5809a45a5964f36bfadb5182bc82194823a
vacool6 <sameerkittur321@gmail.com>
2023-05-25 11:29:50 +0530

    add/comments

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -12,6 +12,7 @@ export const searchBox: SearchBox = buildSearchBox(headlessEngine);
 
 export const resultList = buildResultList(headlessEngine);
 
+// Facets
 export const facet1 = buildFacet(headlessEngine, {
   options: { field: "ec_category" },
 });
@@ -20,8 +21,10 @@ export const facet2 = buildFacet(headlessEngine, {
   options: { field: "ec_name" },
 });
 
+// Pagination
 export const pager = buildPager(headlessEngine);
 
+// Instant results
 export const instantResults = buildInstantResults(headlessEngine, {
   options: { maxResultsPerQuery: 1 },
 });
